856cc94206d24ea469f2e25a795be40abd605cde
gagegolden00 <malcolm.golden0@gmail.com>
2023-09-19 07:48:09 -0500

    creating! the migration

--- db/migrate/20230919123733_create_active_record_methods.rb
+++ db/migrate/20230919123733_create_active_record_methods.rb
@@ -1,7 +1,8 @@
 class CreateActiveRecordMethods < ActiveRecord::Migration[7.0]
   def change
     create_table :active_record_methods do |t|
-
+      t.string :name, null: false
+      t.text :description, null: false
       t.timestamps
     end
   end
